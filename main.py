e4376e4b180a8bb42253f4b34498c6e28a2472c9
Sophi16 <amaro.sophia@escola.pr.gov.br>
2026-09-25 11:06:45 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from codifica import codifica
+
+texto = input("Digite o texto: ")
+deslocamento = int(input("Digite o deslocamento: "))
+
+resultado = codifica(texto, deslocamento)
+
+print("Texto codificado:", resultado)
+
